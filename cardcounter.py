4c5960b1e8a18fc6070f78248f39932cbd8b8342
lukegries <ljgries@gmail.com>
2023-12-10 19:31:30 -0600

    improved some logic

--- cardcounter.py
+++ cardcounter.py
@@ -92,7 +92,7 @@ class ValueIterationAgent:
         self.temperature_values = temperature_values
         self.gamma = gamma
         self.theta = theta
-        self.state_values = np.zeros((32, 11, 2, 10))  # States with temperature
+        self.state_values = np.zeros((32, 11, 2, 10), dtype=float)  # States with temperature (Your Hand)(Dealer showing)(usable ace)(temperature)
         self.policy = np.zeros((32, 11, 2, 10), dtype=int)  # Policy with temperature
 
     def value_iteration(self, env):
@@ -103,7 +103,10 @@ class ValueIterationAgent:
                     for usable_ace in range(2):
                         for temperature in range(10):
                             v_old = self.state_values[player_sum, dealer_showing, usable_ace, temperature]
+                            print(self.state_values.shape)
+                            print(self.state_values[player_sum, dealer_showing, usable_ace, temperature])
                             v_new = self.evaluate_actions(env, player_sum, dealer_showing, usable_ace, temperature)
+                            print(self.state_values.shape)
                             self.state_values[player_sum, dealer_showing, usable_ace, temperature] = v_new
                             delta = max(delta, abs(v_old - v_new))
             if delta < self.theta:
@@ -115,22 +118,53 @@ class ValueIterationAgent:
         for action in range(env.action_space.n):
             actions_values[action] = self.calculate_state_value(player_sum, dealer_showing, usable_ace, action, temperature)
         return np.max(actions_values)
+    
+    def _calculate_dealer_odds(self, dealer_showing, temperature, dealer_odds, weight = 1.0):
+        for card_value, prob in enumerate(self.temperature_values[temperature]):
+                dealer_sum = dealer_showing + card_value + 1
+                if dealer_sum in dealer_odds:
+                    dealer_odds[dealer_sum] += weight * prob
+                else:
+                    if dealer_sum < 21:
+                        self._calculate_dealer_odds(dealer_sum, temperature, dealer_odds, weight = prob)
 
     def calculate_state_value(self, player_sum, dealer_showing, usable_ace, action, temperature):
         if action == 0:  # stick
             # The reward for sticking is independent of the temperature
-            return cmp(player_sum, dealer_showing)  # Compare player sum and dealer showing card
+            expected_reward = 0
+            dealer_odds = {17: 0.0, 18: 0.0, 19: 0.0, 20: 0.0, 21: 0.0}
+            bust = 1.0
+            self._calculate_dealer_odds(dealer_showing, temperature, dealer_odds)
+            for dealer_hand_value in dealer_odds:
+                bust -= dealer_odds[dealer_hand_value]
+            
+            expected_reward = 0.0
+            
+            for dealer_hand_value in dealer_odds:
+                if dealer_hand_value > player_sum:
+                    expected_reward -= 1 * dealer_odds[dealer_hand_value]
+                elif dealer_hand_value == player_sum:
+                    expected_reward += 0
+                elif dealer_hand_value > player_sum:
+                    expected_reward += 1 * dealer_odds[dealer_hand_value]
+            
+            expected_reward += 1 * bust
+            
+            return expected_reward
         else:  # hit
             # Calculate the expected reward for hitting based on temperature-dependent values
             expected_reward = 0
-            for card_value, value in enumerate(self.temperature_values[temperature]):
+            for card_value, prob in enumerate(self.temperature_values[temperature]):
                 new_sum = player_sum + card_value + 1  # card_value is 0-indexed, card values start from 1
+                if usable_ace: # Choose state with higher utility
+                    if self.state_values[new_sum, dealer_showing, 1, temperature] < self.state_values[new_sum + 10, dealer_showing, 1, temperature]:
+                        new_sum += 10
                 if new_sum > 21:
                     reward = -1  # bust
                 else:
                     # Recursively calculate the value of the new state
                     reward = self.state_values[new_sum, dealer_showing, int(new_sum <= 21 and (usable_ace or card_value == 0)), temperature]
-                expected_reward += value * reward
+                expected_reward += prob * reward
             return expected_reward
 
     def extract_policy(self, env):
